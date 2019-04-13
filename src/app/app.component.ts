07be5d2187772b8d56694a8533f0f266787868b4
Akshay-Kumar-Singh <akshayrana707@gmail.com>
2019-04-13 23:58:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mytestapp';
+  counter = 15;
+  mm: any = '00';
+  ss: any = '15';
+  showResend = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.timecheck();
+    }, 500);
+  }
+
+  convertTime(time) {
+    this.mm = Math.floor(time / 60);
+    this.ss = time % 60;
+    if (this.mm < 10 && this.ss >= 10) {
+      this.mm = '0' + this.mm;
+      return this.mm + this.ss;
+    } else if (this.mm > 10 && this.ss < 10) {
+      this.ss = '0' + this.ss;
+      return this.mm + this.ss;
+    } else if (this.mm < 10 && this.ss < 10) {
+      this.mm = '0' + this.mm;
+      this.ss = '0' + this.ss;
+      return this.mm + this.ss;
+    }
+    return this.mm + ':' + this.ss;
+  }
+
+  resend() {
+    this.showResend = true;
+    if (!this.counter) {
+      this.counter = 15;
+      setTimeout(() => {
+        this.timecheck();
+      }, 500);
+    }
+    // timer.html();
+  }
+
+  timecheck() {
+    const turnOff = setTimeout(() => {
+      this.timecheck();
+    }, 1000);
+    this.convertTime(--this.counter);
+    if (!this.counter) {
+      clearTimeout(turnOff);
+      this.showResend = false;
+    }
+  }
 }
